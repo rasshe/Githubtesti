4539018caff665e95d7b2d3ace7a4bc3e0d3b495
rasshe <rasmus.laug97@gmail.com>
2018-02-20 16:53:40 +0200

    testing build
    class

---
+++ Car.py
@@ -0,0 +1,14 @@
+
+
+
+class car():
+    
+    def __init__(self,txt,col):
+        self.txt = txt
+        self.col = col
+        
+    def get_txt(self):
+        return self.txt
+    
+    def get_col(self):
+        return self.col
--- main.py
+++ main.py
@@ -1,12 +1,26 @@
+from Car import car
 
-
-i=0
-
-print("Test Start Toinen")
-
-while i<=100:
-    print(i, ". Test Toinen")
-    i=i+1
+def Test():
+    i=0
     
-print("Test Done Toinen")
+    print("Test Start Toinen")
     
+    while i<=100:
+        print(i, ". Test Toinen")
+        i=i+1
+        
+    print("Test Done Toinen")
+    
+    
+    
+def main():
+    Test()
+    uus = car("Olaa", 20)
+    print("|----------------|")
+    print(uus.get_col)
+    print("|----------------|")
+    print(uus.get_txt)
+    print("|----------------|")
+    
+    
+main()
